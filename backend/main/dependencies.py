40b9c0ef36e04e115026194acd6dc9e7faf1d632
Albert Fazlyev <albert.fz@yandex.ru>
2023-11-25 02:48:08 +0500

    move deps to separate file

---
+++ backend/main/dependencies.py
@@ -0,0 +1,50 @@
+from typing import Annotated
+from fastapi import Depends
+
+import aiosqlite
+
+from main.services.processed_messages_service import ProcessedMessagesService
+from main.services.router_service import RouterService
+from main.repositories.processed_message_repository import ProcessedMessageRepository
+from main.repositories.router_repository import RouterRepository
+
+
+async def get_db():
+    db = await aiosqlite.connect("dbstorage/database.sqlite")
+    try:
+        yield db
+    finally:
+        await db.close()
+
+async def get_processed_messages_repository(
+    db: Annotated[aiosqlite.Connection, Depends(get_db)]
+) -> ProcessedMessageRepository:
+    return ProcessedMessageRepository(db)
+
+
+async def get_router_repository(
+    db: Annotated[aiosqlite.Connection, Depends(get_db)]
+) -> RouterRepository:
+    return RouterRepository(db)
+
+
+async def get_processed_messages_service(
+    processed_message_repository: Annotated[
+        ProcessedMessageRepository, 
+        Depends(get_processed_messages_repository)
+    ]
+) -> ProcessedMessagesService:
+    return ProcessedMessagesService(processed_message_repository)
+
+
+async def get_router_service(
+    router_repository: Annotated[
+        RouterRepository,
+        Depends(get_router_repository)
+    ],
+    processed_messages_repository: Annotated[
+        ProcessedMessageRepository,
+        Depends(get_processed_messages_repository)
+    ]
+    ) -> RouterService:
+    return RouterService(router_repository, processed_messages_repository)
--- backend/main/routes.py
+++ backend/main/routes.py
@@ -1,65 +1,9 @@
 from typing import Annotated
 from fastapi import APIRouter, Depends
-from main.services.processed_messages_service import ProcessedMessagesService
+from main.dependencies import get_router_service
 from main.services.router_service import RouterService
-from main.repositories.processed_message_repository import ProcessedMessageRepository
-from main.repositories.router_repository import RouterRepository
-
-import aiosqlite
-
-"""
-async def get_db():
-    db = await aiosqlite.connect("database/database.sqlite")
-    try:
-        yield db
-    finally:
-        await db.close()
-"""
-
 
 router = APIRouter()
-
-async def get_db():
-    db = await aiosqlite.connect("dbstorage/database.sqlite")
-    try:
-        yield db
-    finally:
-        await db.close()
-
-
-async def get_processed_messages_repository(
-    db: Annotated[aiosqlite.Connection, Depends(get_db)]
-) -> ProcessedMessageRepository:
-    return ProcessedMessageRepository(db)
-
-
-async def get_router_repository(
-    db: Annotated[aiosqlite.Connection, Depends(get_db)]
-) -> RouterRepository:
-    return RouterRepository(db)
-
-
-async def get_processed_messages_service(
-    processed_message_repository: Annotated[
-        ProcessedMessageRepository, 
-        Depends(get_processed_messages_repository)
-    ]
-) -> ProcessedMessagesService:
-    return ProcessedMessagesService(processed_message_repository)
-
-
-async def get_router_service(
-    router_repository: Annotated[
-        RouterRepository,
-        Depends(get_router_repository)
-    ],
-    processed_messages_repository: Annotated[
-        ProcessedMessageRepository,
-        Depends(get_processed_messages_repository)
-    ]
-    ) -> RouterService:
-    return RouterService(router_repository, processed_messages_repository)
-
 
 @router.get("/api/rules")
 async def root(router_service: Annotated[RouterService, Depends(get_router_service)]):
